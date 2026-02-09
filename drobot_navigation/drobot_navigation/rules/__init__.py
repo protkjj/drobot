e42a66ead202dc2d05fef52a11055fed430d70d3
essong2220 <lesong2003@naver.com>
2026-02-09 14:47:25 +0900

    delete: 필요 없는 폴더 삭제

--- drobot_navigation/drobot_navigation/rules/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Rule engine module."""
-from drobot_navigation.rules.engine import RuleEngine
-
-__all__ = ["RuleEngine"]
--- drobot_simulation/drobot_simulation/navigation/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Navigation module for collision handling and recovery behaviors.
-
-Provides collision avoidance and recovery maneuvers for exploration.
-"""
-from .collision import CollisionHandler
-from .recovery import RecoveryManager
-
-__all__ = ['CollisionHandler', 'RecoveryManager']
